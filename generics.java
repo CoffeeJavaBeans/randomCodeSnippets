26b2b66af603e825d3cec4a4ddeb80dd63f824a7
Sarah Anatoli <47230529+CoffeeJavaBeans@users.noreply.github.com>
2024-07-09 23:26:20 -0400

    Create generics.java

---
+++ generics.java
@@ -0,0 +1,19 @@
+/*https://www.hackerrank.com/challenges/java-generics/problem?isFullScreen=true
+Today I chose generics, because I never actually tried this with Java. Other languages like
+Python tend to allow this implicitly without explicitly having to define them. 
+So I found this as an idea on how to do this.
+https://www.tutorialspoint.com/java/java_generics.htm
+https://www.baeldung.com/java-store-scanner-input-in-array
+https://www.baeldung.com/java-generics
+
+So, I also found the naming conventions on Stack Overflow for refernce.
+E - Element (used extensively by the Java Collections Framework)
+K - Key
+N - Number
+T - Type
+V - Value
+S,U,V etc. - 2nd, 3rd, 4th types
+
+
+I need to step away, but I will be back to work on this tomorrow! 
+*/
