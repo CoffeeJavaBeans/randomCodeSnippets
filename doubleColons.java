3ffc0c4c3f99dada7ca3eec931128dda57bc3b76
Sarah Anatoli <47230529+CoffeeJavaBeans@users.noreply.github.com>
2024-07-12 10:43:37 -0400

    Create doubleColons.java

---
+++ doubleColons.java
@@ -0,0 +1,10 @@
+/*
+ The other day, I couldn't finish the study I wanted to do with Generics, but also wound up wondering about double
+ colon notation ::
+ This geeksforgeeks article was super helpful to understand https://www.geeksforgeeks.org/double-colon-operator-in-java/
+
+ So, it's called a method reference operator and they behave exactly like lambda functions.
+ Lambad fucntion syntax: stream.forEach( s-> System.out.println(s));
+ Method reference syntax: <Class name>::<method name>
+ 
+*/
